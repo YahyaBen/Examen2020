e81134ebde863e49ac619b81132911404f2a5766
Yahya BENABDALLAH <Hyrkul@outlook.com>
2020-07-20 16:10:06 +0200

    Question  7 - a - i & ii : Modification dans le model Patient avec les annotations !

--- Examen_YB/Examen_YB/Models/Patient.cs
+++ Examen_YB/Examen_YB/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace Examen_YB.Models
     public class Patient
     {
         public int Id { get; set; }
+        [Required] // Obligatoire
+        [StringLength(30, MinimumLength = 3)] // pour {3,30}
+        [DataType(DataType.Text)] // pour la forme [A-Z-a-z]
         public  string Nom { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [DataType(DataType.Text)]
         public string Prenom { get; set; }
+        [Required]// Obligatoire
+        [Range(0, Double.PositiveInfinity)] // Positive strict.
         public int Age { get; set; }
         public string Sexe { get; set; }
         public bool Deces { get; set; }
