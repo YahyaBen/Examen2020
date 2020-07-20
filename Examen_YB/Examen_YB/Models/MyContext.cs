99ec34473bce5498debe5d4a4434c2485d6a16c2
Yahya BENABDALLAH <Hyrkul@outlook.com>
2020-07-20 18:05:45 +0200

    Question 9 : Identity

--- Examen_YB/Examen_YB/Models/MyContext.cs
+++ Examen_YB/Examen_YB/Models/MyContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Examen_YB.Models
 {
-    public class MyContext : DbContext
+    public class MyContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Historique> Historiques { get; set; }
