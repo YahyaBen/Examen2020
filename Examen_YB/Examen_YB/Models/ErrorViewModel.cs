9c5eaab6a4f13ba21542eaf9e2afd246412c813f
Yahya BENABDALLAH <Hyrkul@outlook.com>
2020-07-20 11:33:22 +0200

    Initial commit :)

---
+++ Examen_YB/Examen_YB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Examen_YB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
